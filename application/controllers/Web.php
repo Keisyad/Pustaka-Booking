68324e7c9205654149d31d59513cae0db851727d
keisyad <keisyad210@gmail.com>
2023-10-23 10:38:40 +0700

    penambahan isi

---
+++ application/controllers/Web.php
@@ -0,0 +1,39 @@
+<?php
+defined('BASEPATH') or exit ('no direct script access allowed');
+class Web extends CI_Controller{
+ function __construct(){
+ parent::__construct();
+ }
+ public function index(){
+ $data['judul'] = "halaman depan";
+ $this->load->view('v_header',$data);
+ $this->load->view('v_index',$data);
+ $this->load->view('v_footer',$data);
+}
+
+public function about() 
+    { 
+        $data['judul'] = "Halaman About"; 
+        $this->load->view('v_header', $data); 
+        $this->load->view('v_about', $data); 
+        $this->load->view('v_footer', $data); 
+    } 
+
+    public function BukuTamu()
+    {
+        $data['judul'] = "Halaman About";
+        $this->load->view('v_header', $data);
+        $this->load->view('v_BukuTamu', $data);
+        $this->load->view('v_footer', $data);
+    }
+    public function cetak(){
+        $data = [
+    'nama' => $this->input->post('nama'),
+    'email' => $this->input->post('email'),
+    'komentar' => $this->input->post('komentar'),
+        ];
+    $this->load->view('v_header', $data);
+    $this->load->view('v_datatamu', $data);
+    $this->load->view('v_footer', $data);
+    }
+}
